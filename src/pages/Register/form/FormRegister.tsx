9e529e1ac7e72ecdcbfe19057f35ba62b7bc7002
Mateus Elias Vieira <mateusifg16@gmail.com>
2023-11-13 13:56:50 -0300

    adicionado mudanças no código de correções de erros

--- src/pages/Register/form/FormRegister.tsx
+++ src/pages/Register/form/FormRegister.tsx
@@ -1,143 +1,143 @@
-import React, {useState} from "react"
-import {BsCupHotFill} from "react-icons/bs";
+import React, { useState } from "react"
+import { BsCupHotFill } from "react-icons/bs";
 import "./FormRegister.css"
 import MyInput from "../../../components/myinput/MyInput.tsx";
 import MyTextArea from "../../../components/mytextarea/MyTextArea.tsx";
 import axios from "axios";
 import Alert from "../../../components/alerts/Alert.tsx";
-import {ENDPOINT_NEW_USER} from "../../../global/URLs.ts";
+import { ENDPOINT_NEW_USER } from "../../../global/URLs.ts";
 
-const FormRegister:React.FC = () => {
+const FormRegister: React.FC = () => {
 
-    const[status,setStatus]=useState(0)
-    const[loading,setLoading]=useState(false)
-    const[response,setResponse]=useState("")
+    const [status, setStatus] = useState(0)
+    const [loading, setLoading] = useState(false)
+    const [response, setResponse] = useState("")
     const [data, setData] = useState({
         name: "",
         email: "",
         username: "",
         password: "",
         about: "",
-        photo: "",
+        image: "",
     });
 
-    const sendMyData = ()=>{
+    const sendMyData = () => {
         let count = 0;
         setStatus(0)
         setResponse("")
         Object.values(data).forEach((value) => {
-            if(!value){
+            if (!value) {
                 count += 1
             }
         });
-        if(count === 0){
+        if (count === 0) {
             setLoading(true)
-            axios.post(ENDPOINT_NEW_USER,data)
-                .then((response)=>{
+            axios.post(ENDPOINT_NEW_USER, data)
+                .then((response) => {
                     setLoading(false)
                     setResponse(response.data.message)
                     setStatus(response.status)
-                     if(status === 200){
-                         setTimeout(()=>{ setStatus(0); window.location.href = "/" },5000)
-                     }
+
+                    if (status === 200) {
+                        setTimeout(() => { setStatus(0); window.location.href = "/" }, 5000)
+                    }
                 })
-                .catch((err)=>{
+                .catch((err) => {
                     setResponse(err.response.data.message)
                     setStatus(err.response.status)
                     setLoading(false)
                 })
-           // console.log("status http = "+status)
-        }else{
-            alert("Informe todos os campos")
+        } else {
+            setStatus(400)
+            setResponse("Informe todos os campos")
         }
 
     }
-    return(
-         <div className="form-create-account">
-                <legend>Welcome user</legend>
-                {
-                    loading
-                        ?
-                        (
-                            <div className={"box-loading"}>
-                                <div className="spinner-grow" role="status">
-                                    <span className="sr-only"></span>
-                                </div>
-                                <p>Sending confirmation email...</p>
+    return (
+        <div className="form-create-account">
+            <legend>Welcome user</legend>
+            {
+                loading
+                    ?
+                    (
+                        <div className={"box-loading"}>
+                            <div className="spinner-grow" role="status">
+                                <span className="sr-only"></span>
                             </div>
+                            <p>Sending confirmation email...</p>
+                        </div>
 
-                        )
-                        :
-                        ""
-                }
+                    )
+                    :
+                    ""
+            }
 
-             {status !== 0 ? setTimeout(() => {
-                 setStatus(0)
-                 setResponse("")
-                 setData({
-                     name: "",
-                     email: "",
-                     username: "",
-                     password: "",
-                     about: "",
-                     photo: "",
-                 })
-             }, 5000) && (
-                 <Alert typeAlert={status === 200 ? "alert alert-success" : "alert alert-danger"}
-                        message={response}/>) : ""}
+            {status !== 0 ? setTimeout(() => {
+                setStatus(0)
+                setResponse("")
+                setData({
+                    name: "",
+                    email: "",
+                    username: "",
+                    password: "",
+                    about: "",
+                    image: "",
+                })
+            }, 10000) && (
+                    <Alert typeAlert={status === 200 ? "alert alert-success" : "alert alert-danger"}
+                        message={response} />) : ""}
 
-                <div className="box-group-inputs-flex">
-                    <MyInput
-                        classGroup="box-label-input space-right"
-                        textLabel="Name *"
-                        typeInput="text"
-                        valueInput={data.name}
-                        fun={(newValue)=>{setData({...data, name:newValue})}}/>
-                    <MyInput
-                        classGroup="box-label-input"
-                        textLabel="Email"
-                        typeInput="email"
-                        valueInput={data.email}
-                        fun={(newValue)=>{setData({...data, email:newValue})}}/>
-                </div>
-                <div className="box-group-inputs-flex">
-                    <MyInput
-                        classGroup="box-label-input space-right"
-                        textLabel="Username *"
-                        typeInput="text"
-                        valueInput={data.username}
-                        fun={(newValue)=>{setData({...data, username:newValue})}}/>
-                    <MyInput
-                        classGroup="box-label-input"
-                        textLabel="Password *"
-                        typeInput="password"
-                        valueInput={data.password}
-                        fun={(newValue)=>{setData({...data, password:newValue})}}/>
-                </div>
-                 <MyTextArea
-                     classGroup="mb-3"
-                     textLabel="About"
-                     rows={5}
-                     valueTextArea={data.about}
-                     fun={(newValue)=>{setData({...data, about:newValue})}}/>
-
+            <div className="box-group-inputs-flex">
                 <MyInput
-                    classGroup="mb-3"
-                    textLabel="Your photo"
-                    typeInput="file"
-                    valueInput=""
-                    fun={(newValue)=>{setData({...data, photo:newValue})}}/>
+                    classGroup="box-label-input space-right"
+                    textLabel="Name *"
+                    typeInput="text"
+                    valueInput={data.name}
+                    fun={(newValue) => { setData({ ...data, name: newValue }) }} />
+                <MyInput
+                    classGroup="box-label-input"
+                    textLabel="Email"
+                    typeInput="email"
+                    valueInput={data.email}
+                    fun={(newValue) => { setData({ ...data, email: newValue }) }} />
+            </div>
+            <div className="box-group-inputs-flex">
+                <MyInput
+                    classGroup="box-label-input space-right"
+                    textLabel="Username *"
+                    typeInput="text"
+                    valueInput={data.username}
+                    fun={(newValue) => { setData({ ...data, username: newValue }) }} />
+                <MyInput
+                    classGroup="box-label-input"
+                    textLabel="Password *"
+                    typeInput="password"
+                    valueInput={data.password}
+                    fun={(newValue) => { setData({ ...data, password: newValue }) }} />
+            </div>
+            <MyTextArea
+                classGroup="mb-3"
+                textLabel="About"
+                rows={5}
+                valueTextArea={data.about}
+                fun={(newValue) => { setData({ ...data, about: newValue }) }} />
 
-             <button
-                    className="btn btn-dark"
-                    onClick={(event)=>
-                    {
-                        event.preventDefault()
-                        sendMyData()
-                    }}>Register <BsCupHotFill/>
-                </button>
-                <a href="/">Sign-in</a>
-         </div>
+            <MyInput
+                classGroup="mb-3"
+                textLabel="Your photo"
+                typeInput="file"
+                valueInput=""
+                fun={(newValue) => { setData({ ...data, image: newValue }) }} />
+
+            <button
+                className="btn btn-dark"
+                onClick={(event) => {
+                    event.preventDefault()
+                    sendMyData()
+                }}>Register <BsCupHotFill />
+            </button>
+            <a href="/">Sign-in</a>
+        </div>
     )
 }
 
