be8956ab5170197b9013e488ea9cfa3cc659a26e
Mateus Elias Vieira <mateusifg16@gmail.com>
2023-11-25 14:55:57 -0300

    realizada pequenas modificações em estilos css

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Register.css"
 import FormRegister from "./form/FormRegister.tsx";
+import "./Register.css"
 
-const Login:React.FC = () => {
-    return(
-        <section className="section-login">
-            <FormRegister/>
+const Login: React.FC = () => {
+    return (
+        <section className="section-register">
+            <FormRegister />
         </section>
     )
 }
 
 
-export  default Login
+export default Login
