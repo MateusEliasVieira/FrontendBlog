645fe31b61d1c4fb006e9b6ff3bdc7dcac4721b2
Mateus Elias Vieira <mateusifg16@gmail.com>
2023-11-30 21:27:51 -0300

    Concertado cadastro de usuário para enviar sua foto

--- src/pages/Register/form/FormRegister.tsx
+++ src/pages/Register/form/FormRegister.tsx
@@ -9,10 +9,14 @@ import { ENDPOINT_NEW_USER } from "../../../global/URLs.ts";
 
 const FormRegister: React.FC = () => {
 
-    const [status, setStatus] = useState(0)
     const [loading, setLoading] = useState(false)
-    const [response, setResponse] = useState("")
-    const [responseError, setResponseError] = useState([])
+    const [nameError, setNameError] = useState("")
+    const [emailError, setEmailError] = useState("")
+    const [usernameError, setUsernameError] = useState("")
+    const [passwordError, setPasswordError] = useState("")
+    const [aboutError, setAboutError] = useState("")
+    const [imageError, setImageError] = useState("")
+    const [arq, setArq] = useState<File | null>(null);
     const [data, setData] = useState({
         name: "",
         email: "",
@@ -22,36 +26,103 @@ const FormRegister: React.FC = () => {
         image: "",
     });
 
-    const sendMyData = () => {
-        // setStatus(0);
-        // setResponse("");
-        // setResponseError([]);
-
-        // try {
-        //     const response = await axios.post(ENDPOINT_NEW_USER, data);
-        //     setResponse(response.data.message);
-        //     setStatus(response.status);
-
-        //     if (response.status === 200) {
-        //         setLoading(false);
-        //         setTimeout(() => window.location.href = "/", 5000);
-        //     }
-        // } catch (error: Error) {
-        //     console.error(error);
-        //     if (error.response) {
-        //         setResponseError(error.response.data);
-        //         setStatus(error.response.status);
-        //     } else if (error.request) {
-        //         setResponseError(["Erro de rede. Tente novamente mais tarde."]);
-        //         setStatus(500);
-        //     } else {
-        //         setResponseError(["Ocorreu um erro inesperado. Tente novamente mais tarde."]);
-        //         setStatus(500);
-        //     }
-
-        //     setLoading(false);
-        // }
+    const clearErrors = () => {
+        setNameError("");
+        setEmailError("");
+        setUsernameError("");
+        setPasswordError("");
+        setAboutError("");
+        setImageError("");
     };
+
+    const isAnyFieldEmpty = () => {
+        for (const key in data) {
+            if (data.hasOwnProperty(key) && data[key].trim() === "") {
+                return true; // Retorna true se encontrar algum campo vazio
+            }
+        }
+        return false; // Retorna false se todos os campos estiverem preenchidos
+    };
+
+    const checkFieldWithProblem = (field: string, message: string) => {
+        switch (field) {
+            case "name":
+                setNameError(message);
+                break;
+            case "email":
+                setEmailError(message);
+                break;
+            case "username":
+                setUsernameError(message);
+                break;
+            case "password":
+                setPasswordError(message);
+                break;
+            case "about":
+                setAboutError(message);
+                break;
+            case "image":
+                setImageError(message);
+                break;
+        }
+    }
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = event.target.files?.[0];
+        console.log(selectedFile)
+        setArq(selectedFile);
+    };
+
+    const imageToBase64 = (file: File): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+
+            reader.onloadend = () => {
+                const base64String = reader.result as string;
+                resolve(base64String);
+            };
+
+            reader.onerror = (error) => {
+                reject(error);
+            };
+
+            reader.readAsDataURL(file);
+        });
+    };
+
+    const setImageConverted = async () => {
+        if (arq instanceof File) {
+            const base64Image = await imageToBase64(arq);
+            setData({ ...data, image: base64Image });
+        }
+    }
+
+    const sendMyData = async () => {
+        await setImageConverted()
+        if (!isAnyFieldEmpty()) {
+            setLoading(true);
+            axios.post(ENDPOINT_NEW_USER, data)
+                .then((response) => {
+                    clearErrors()
+                    setLoading(false)
+                    alert(response?.data?.message)
+                    window.location.href = "/"
+                })
+                .catch((error) => {
+                    setLoading(false)
+                    clearErrors()
+                    if (error?.response?.data?.list != undefined) {
+                        if (error?.response?.data?.list.length > 0) {
+                            error?.response?.data?.list.map((item: object) => {
+                                checkFieldWithProblem(item?.nameField, item?.message)
+                            })
+                        }
+                    }
+                    alert(error?.response?.data?.title)
+                })
+        } else {
+            alert("Informe todos os campos!")
+        }
+    }
 
     return (
         <div className="form-create-account">
@@ -64,7 +135,7 @@ const FormRegister: React.FC = () => {
                             <div className="spinner-grow" role="status">
                                 <span className="sr-only"></span>
                             </div>
-                            <p> Enviando email de confirmação... </p>
+                            <p> Aguarde um momento... </p>
                         </div>
 
                     )
@@ -83,7 +154,7 @@ const FormRegister: React.FC = () => {
                             typeInput="text"
                             valueInput={data.name}
                             fun={(newValue) => { setData({ ...data, name: newValue }) }} />
-                        <p className="p-error">Erros</p>
+                        <p className="p-error">{nameError}</p>
                     </div>
 
                     <div className="box-group-input-p">
@@ -93,7 +164,7 @@ const FormRegister: React.FC = () => {
                             typeInput="email"
                             valueInput={data.email}
                             fun={(newValue) => { setData({ ...data, email: newValue }) }} />
-                        <p className="p-error">Erros</p>
+                        <p className="p-error">{emailError}</p>
                     </div>
 
                 </div>
@@ -107,7 +178,7 @@ const FormRegister: React.FC = () => {
                         typeInput="text"
                         valueInput={data.username}
                         fun={(newValue) => { setData({ ...data, username: newValue }) }} />
-                    <p className="p-error">Erros</p>
+                    <p className="p-error">{usernameError}</p>
                 </div>
                 <div className="box-group-input-p">
                     <MyInput
@@ -116,7 +187,7 @@ const FormRegister: React.FC = () => {
                         typeInput="password"
                         valueInput={data.password}
                         fun={(newValue) => { setData({ ...data, password: newValue }) }} />
-                    <p className="p-error">Erros</p>
+                    <p className="p-error">{passwordError}</p>
                 </div>
             </div>
             <div className="box-group-input-p textarea">
@@ -126,16 +197,12 @@ const FormRegister: React.FC = () => {
                     rows={5}
                     valueTextArea={data.about}
                     fun={(newValue) => { setData({ ...data, about: newValue }) }} />
-                <p className="p-error">Erros</p>
+                <p className="p-error">{aboutError}</p>
             </div>
             <div className="box-group-input-p">
-                <MyInput
-                    classGroup="mb-3"
-                    textLabel="Sua foto *"
-                    typeInput="file"
-                    valueInput=""
-                    fun={(newValue) => { setData({ ...data, image: newValue }) }} />
-                <p className="p-error">Erros</p>
+                <label>Sua foto *</label>
+                <input className="mb-3" type="file" onChange={handleFileChange} />
+                <p className="p-error">{imageError}</p>
             </div>
 
             <button
